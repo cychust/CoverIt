cb1525f3dc5efb08e25487326b3dd591ca8ddec2
cychust <u201614719@hust.edu.cn>
2018-06-08 00:25:53 +0800

    限制反馈间隔

--- app/src/main/java/net/bingyan/coverit/ui/reciteother/FeedBackActivity.kt
+++ app/src/main/java/net/bingyan/coverit/ui/reciteother/FeedBackActivity.kt
@@ -27,7 +27,8 @@ class FeedBackActivity : AppCompatActivity() {
     private lateinit var btnSubmit: Button
     private lateinit var tvbtn: TextView
     private lateinit var appBarLayout: AppBarLayout
-    private lateinit var statusBarView: View;
+    private lateinit var statusBarView: View
+    private var time = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed_back)
@@ -57,42 +58,43 @@ class FeedBackActivity : AppCompatActivity() {
         //titleBar.setTitle("反馈")
         //  titleBar.setTitleColor(ContextCompat.getColor(this@FeedBackActivity,R.color.title_white))
         btnSubmit.onClick {
-            if (!etSuggestion.text.isEmpty()) {
-                var paras = hashMapOf<String, String>()
-                paras["info"] = etSuggestion.text.toString() + etAddress.text.toString()
-                NetUtils.getInstance().postDataAsynToNet("http://cqmu.wangluyuan.cc/feedback/", paras, object : NetUtils.MyNetCall {
-                    @Throws(IOException::class)
-                    override fun success(call: Call?, response: Response?) {
-                        var handler: Handler = Handler(Looper.getMainLooper())
-                        handler.post({
-                            //Log.d("message",response?.code().toString())
-                            if (!response?.body()?.string()?.trim().equals("{\"state\": \"ok\"}")){
-                                Toast.makeText(this@FeedBackActivity, "发送错误，请联系相关人员", Toast.LENGTH_SHORT).show()
-                                finish()
-                            }else {
-                             //   Log.d("feedback_response", response?.body()?.string())
-                                val toast = CustomToast(this@FeedBackActivity, LayoutInflater.from(this@FeedBackActivity).inflate(R.layout.popup_view, null, false), Toast.LENGTH_SHORT)
-                                toast.show()
-                                finish()
+            if (time != 0L && System.currentTimeMillis() - time >= 3 * 1000)
+                if (!etSuggestion.text.isEmpty()) {
+                    var paras = hashMapOf<String, String>()
+                    paras["info"] ="[背它Android反馈]" +etSuggestion.text.toString() +"联系方式:"+ etAddress.text.toString()
+                    NetUtils.getInstance().postDataAsynToNet("http://cqmu.wangluyuan.cc/feedback/", paras, object : NetUtils.MyNetCall {
+                        @Throws(IOException::class)
+                        override fun success(call: Call?, response: Response?) {
+                            var handler: Handler = Handler(Looper.getMainLooper())
+                            handler.post({
+                                //Log.d("message",response?.code().toString())
+                                if (!response?.body()?.string()?.trim().equals("{\"state\": \"ok\"}")) {
+                                    Toast.makeText(this@FeedBackActivity, "发送错误，请联系相关人员", Toast.LENGTH_SHORT).show()
+                                    finish()
+                                } else {
+                                    //   Log.d("feedback_response", response?.body()?.string())
+                                    val toast = CustomToast(this@FeedBackActivity, LayoutInflater.from(this@FeedBackActivity).inflate(R.layout.popup_view, null, false), Toast.LENGTH_SHORT)
+                                    toast.show()
+                                    finish()
+                                }
                             }
+                            )
+
                         }
-                        )
 
-                    }
+                        @Throws(IOException::class)
+                        override fun failed(call: Call?, e: IOException?) {
+                            //To change body of created functions use File | Settings | File Templates.
+                            var handler: Handler = Handler(Looper.getMainLooper())
+                            handler.post({
+                                Toast.makeText(this@FeedBackActivity, "网络错误,请稍后重试", Toast.LENGTH_SHORT).show()
+                            })
+                        }
+                    })
 
-                    @Throws(IOException::class)
-                    override fun failed(call: Call?, e: IOException?) {
-                        //To change body of created functions use File | Settings | File Templates.
-                        var handler: Handler = Handler(Looper.getMainLooper())
-                        handler.post({
-                            Toast.makeText(this@FeedBackActivity, "网络错误,请稍后重试", Toast.LENGTH_SHORT).show()
-                        })
-                    }
-                })
-
-            } else if (etSuggestion.text.isEmpty()) {
-                Toast.makeText(this@FeedBackActivity, "网络错误,请稍后重试", Toast.LENGTH_SHORT).show()
-            }
+                } else if (etSuggestion.text.isEmpty()) {
+                    Toast.makeText(this@FeedBackActivity, "网络错误,请稍后重试", Toast.LENGTH_SHORT).show()
+                }
         }
         tvbtn.isClickable = true
         tvbtn.onClick { finish() }
